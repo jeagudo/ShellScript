ad3b51c51024271aecf23c6bb94684b0b90948b7
J. Enrique Agudo <jeagudo@unex.es>
2018-01-23 19:18:32 +0100

    23/01/2018

--- utiles/procesar_passwdV2.sh
+++ utiles/procesar_passwdV2.sh
@@ -1,14 +1,18 @@
 #/bin/bash
 #J. Enrique Agudo
 #Busca un usuario y muestra su información de /etc/passwd
+#Modificado para que muestre exáctamente al usuario, por ejemplo si buscamos "pepe" 
+#y hay un usuario pepe2 no debería mostrarlo
 if [ $# -ne 1 ]; then
   echo "USO: $0 usuario"
   exit 1
 fi
 
 while read linea; do 
-  if echo $linea | grep $1 > /dev/null; then
-    echo "Nombre:" $(echo $linea | cut -d ":" -f1)
+  usuario=$(echo $linea | cut -d ":" -f1)
+#echo $usuario
+  if [ $usuario = $1 ]; then
+    echo "Nombre:" $usuario
     echo "Password:" $(echo $linea | cut -d ":" -f2)
     echo "UID:" $(echo $linea | cut -d ":" -f3)
     echo "GUID:" $(echo $linea | cut -d ":" -f4)
